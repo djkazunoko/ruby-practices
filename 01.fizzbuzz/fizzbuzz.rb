2bf32948fe0218fc281beac69d83b17bc1c9c77d
kazunoko1606 <kazu.negi0719@gmail.com>
2022-05-31 10:25:37 +0900

    条件分岐をcaseからifに変更

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,12 +1,11 @@
 nums = Range.new(1,20)
 
 nums.each do |num|
-  case
-  when num % 15 == 0
+  if num % 15 == 0
     puts "FizzBuzz" 
-  when num % 3 == 0
+  elsif num % 3 == 0
     puts "Fizz" 
-  when num % 5 == 0
+  elsif num % 5 == 0
     puts "Buzz" 
   else
     puts num
