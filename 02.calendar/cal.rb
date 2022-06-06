0df9c72d253062d584b635baf117ca9c2922a400
kazunoko1606 <djkazunoko@gmail.com>
2022-06-06 10:21:14 +0900

    Dateクラス自体で範囲オブジェクトを作成するように変更、変数名を理解しやすい名前に変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -7,42 +7,42 @@ today = Date.today
 options = ARGV.getopts("", "m:#{today.month}", "y:#{today.year}")
 
 if options["m"].to_i >= 1 && options["m"].to_i <= 12
-  month = options["m"].to_i
+  inputed_month = options["m"].to_i
 else
   puts "cal: #{options["m"]} is neither a month number (1..12) nor a name"
   return
 end
 
 if options["y"].to_i >= 1 && options["y"].to_i <= 9999
-  year = options["y"].to_i
+  inputed_year = options["y"].to_i
 else
   puts "cal: year `#{options["y"]}' not in range 1..9999"
   return
 end
 
-first_date_day = Date.new(year, month).wday
-last_date = Date.new(year, month, -1).day
+first_date = Date.new(inputed_year, inputed_month, 1)
+last_date = Date.new(inputed_year, inputed_month, -1)
 space = "   "
 
-puts "      #{month}月 #{year}"
+puts "      #{inputed_month}月 #{inputed_year}"
 puts "日 月 火 水 木 金 土"
 
-print space * first_date_day
+print space * first_date.wday
 
 def color_reverse(text)
   "\e[30m\e[47m#{text}\e[0m"
 end
 
-Range.new(1,last_date).each do |date|
-  day = Date.new(year, month, date).wday
-  print " " if date <= 9
-  if date == today.day && month == today.month && year == today.year
-    print color_reverse(date)
+(first_date..last_date).each do |full_date|
+  day_of_week = full_date.wday
+  print " " if full_date.day <= 9
+  if full_date == today
+    print color_reverse(full_date.day)
   else
-    print "#{date}"
+    print "#{full_date.day}"
   end
   print " "
-  puts "" if day == 6
+  puts "" if day_of_week == 6
 end
 
 puts ""
