24c8064a010c8ed11377a87f20692b70cb2128cb
kazunoko1606 <djkazunoko@gmail.com>
2022-06-10 11:45:03 +0900

    旧ルールに修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
 scores = score.split(',')
@@ -17,14 +18,21 @@ shots.each_slice(2) do |s|
   frames << s
 end
 
-point = 0
-frames.each do |frame|
-  if frame[0] == 10
-    point += 30
-  elsif frame.sum == 10
-    point += frame[0] + 10
-  else
-    point += frame.sum
+frames_point = []
+frames.each_with_index do |frame, i|
+  frames_point[i] = frame.sum
+  if i < 9
+    if frame[0] == 10
+      frames_point[i] += frames[i + 1][0]
+      frames_point[i] += if frames[i + 1][0] == 10
+                           frames[i + 2][0]
+                         else
+                           frames[i + 1][1]
+                         end
+    elsif frame.sum == 10
+      frames_point[i] += frames[i + 1][0]
+    end
   end
+  frames_point[i] += frames_point[i - 1] if i != 0
 end
-puts point
+puts frames_point.last
