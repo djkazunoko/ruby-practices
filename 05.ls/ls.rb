06ab3d28250f24119b7a8b310a2c12680a2dbdee
kazunoko1606 <djkazunoko@gmail.com>
2022-07-02 10:32:25 +0900

    メソッドを定義

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,20 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-files = Dir.glob('*', base: './')
-number_of_elements = files.size
-max_number_of_columns = 3
-number_of_rows = if (number_of_elements % max_number_of_columns).zero?
-                   number_of_elements / max_number_of_columns
-                 else
-                   (number_of_elements / max_number_of_columns) + 1
-                 end
-column_width = files.map(&:size).max + 2
-
-number_of_rows.times do |i|
-  while i < number_of_elements
-    print files[i].ljust(column_width)
-    i += number_of_rows
+def display_files(max_number_of_columns)
+  files = Dir.glob('*', base: './')
+  number_of_elements = files.size
+  max_number_of_words = files.map(&:size).max
+  number_of_rows = calc_number_of_rows(number_of_elements, max_number_of_columns)
+  number_of_rows.times do |i|
+    while i < number_of_elements
+      print files[i].ljust(max_number_of_words + 2)
+      i += number_of_rows
+    end
+    print "\n"
   end
-  print "\n"
 end
+
+def calc_number_of_rows(number_of_elements, max_number_of_columns)
+  if (number_of_elements % max_number_of_columns).zero?
+    number_of_elements / max_number_of_columns
+  else
+    (number_of_elements / max_number_of_columns) + 1
+  end
+end
+
+display_files(3)
