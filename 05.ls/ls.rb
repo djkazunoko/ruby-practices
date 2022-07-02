25dd5710a844885fe44e68b0913099feffb895c3
kazunoko1606 <djkazunoko@gmail.com>
2022-07-02 09:07:20 +0900

    オプション無しのlsを実装

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
+files = Dir.glob('*', base: './')
+number_of_elements = files.size
+max_number_of_columns = 3
+number_of_rows = if (number_of_elements % max_number_of_columns).zero?
+                   number_of_elements / max_number_of_columns
+                 else
+                   (number_of_elements / max_number_of_columns) + 1
+                 end
+column_width = files.map(&:size).max + 2
+
+number_of_rows.times do |i|
+  while i < number_of_elements
+    print files[i].ljust(column_width)
+    i += number_of_rows
+  end
+  print "\n"
+end
