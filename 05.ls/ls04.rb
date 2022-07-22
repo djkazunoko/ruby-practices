586ff6f1cb82b843bffe9f1f0d29b3e549909f28
kazunoko1606 <djkazunoko@gmail.com>
2022-07-23 08:58:15 +0900

    ファイルモードの表示機能を実装

--- 05.ls/ls04.rb
+++ 05.ls/ls04.rb
@@ -11,8 +11,8 @@ def display_files(params)
 
   if params['l']
     files.each do |file|
-      fs = File.stat(file)
-      file_mode = fs.mode
+      fs = File.lstat(file)
+      file_mode = get_file_mode(fs)
       number_of_links = fs.nlink
       owner_name = Etc.getpwuid(fs.uid).name
       group_name = Etc.getgrgid(fs.gid).name
@@ -41,6 +41,64 @@ def calc_number_of_rows(number_of_elements, max_number_of_columns)
   else
     (number_of_elements / max_number_of_columns) + 1
   end
+end
+
+def get_file_mode(fs)
+  file_mode_numeric = fs.mode.to_s(8).rjust(6, "0")
+  file_type_symbolic = get_file_type_symbolic(fs.ftype)
+  file_permissions_symbolic = get_file_permissions_symbolic(file_mode_numeric)
+  file_mode_symbolic = "#{file_type_symbolic}#{file_permissions_symbolic}"
+end
+
+def get_file_type_symbolic(name)
+  {
+    "fifo" => "p",
+    "characterSpecial" => "c",
+    "directory" => "d",
+    "blockSpecial" => "b",
+    "file" => "-",
+    "link" => "l",
+    "socket" => "s"
+  }[name]
+end
+
+def get_file_permissions_symbolic(file_mode_numeric)
+  file_permissions_symbolic = []
+  file_mode_numeric.slice(3, 3).each_char do |file_permission_numeric|
+    file_permission_symbolic = {
+      "0" => "---",
+      "1" => "--x",
+      "2" => "-w-",
+      "3" => "-wx",
+      "4" => "r--",
+      "5" => "r-x",
+      "6" => "rw-",
+      "7" => "rwx",
+    }[file_permission_numeric]
+    file_permissions_symbolic << file_permission_symbolic
+  end
+
+  if file_mode_numeric.slice(2) == "1"
+    if file_permissions_symbolic[2].slice(2) == "x"
+      file_permissions_symbolic[2] = file_permissions_symbolic[2].gsub(/.$/, "t")
+    else
+      file_permissions_symbolic[2] = file_permissions_symbolic[2].gsub(/.$/, "T")
+    end
+  elsif file_mode_numeric.slice(2) == "2"
+    if file_permissions_symbolic[1].slice(2) == "x"
+      file_permissions_symbolic[1] = file_permissions_symbolic[1].gsub(/.$/, "s")
+    else
+      file_permissions_symbolic[1] = file_permissions_symbolic[1].gsub(/.$/, "S")
+    end
+  elsif file_mode_numeric.slice(2) == "4"
+    if file_permissions_symbolic[0].slice(2) == "x"
+      file_permissions_symbolic[0] = file_permissions_symbolic[0].gsub(/.$/, "s")
+    else
+      file_permissions_symbolic[0] = file_permissions_symbolic[0].gsub(/.$/, "S")
+    end
+  end
+
+  file_permissions_symbolic.join
 end
 
 display_files(params)
