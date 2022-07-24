387b177a6df0cd8a167ca7eb07d66ddd8126b8a2
kazunoko1606 <djkazunoko@gmail.com>
2022-07-25 08:01:22 +0900

    rubocop:文字列にシングルクウォーテーションマークを使用するように修正

--- 05.ls/ls04.rb
+++ 05.ls/ls04.rb
@@ -65,7 +65,7 @@ def calc_number_of_rows(number_of_elements, max_number_of_columns)
 end
 
 def get_file_mode(fs)
-  file_mode_numeric = fs.mode.to_s(8).rjust(6, "0")
+  file_mode_numeric = fs.mode.to_s(8).rjust(6, '0')
   file_type_symbolic = get_file_type_symbolic(fs.ftype)
   file_permissions_symbolic = get_file_permissions_symbolic(file_mode_numeric)
   file_mode_symbolic = "#{file_type_symbolic}#{file_permissions_symbolic}"
@@ -73,13 +73,13 @@ end
 
 def get_file_type_symbolic(name)
   {
-    "fifo" => "p",
-    "characterSpecial" => "c",
-    "directory" => "d",
-    "blockSpecial" => "b",
-    "file" => "-",
-    "link" => "l",
-    "socket" => "s"
+    'fifo' => 'p',
+    'characterSpecial' => 'c',
+    'directory' => 'd',
+    'blockSpecial' => 'b',
+    'file' => '-',
+    'link' => 'l',
+    'socket' => 's'
   }[name]
 end
 
@@ -87,35 +87,35 @@ def get_file_permissions_symbolic(file_mode_numeric)
   file_permissions_symbolic = []
   file_mode_numeric.slice(3, 3).each_char do |file_permission_numeric|
     file_permission_symbolic = {
-      "0" => "---",
-      "1" => "--x",
-      "2" => "-w-",
-      "3" => "-wx",
-      "4" => "r--",
-      "5" => "r-x",
-      "6" => "rw-",
-      "7" => "rwx",
+      '0' => '---',
+      '1' => '--x',
+      '2' => '-w-',
+      '3' => '-wx',
+      '4' => 'r--',
+      '5' => 'r-x',
+      '6' => 'rw-',
+      '7' => 'rwx',
     }[file_permission_numeric]
     file_permissions_symbolic << file_permission_symbolic
   end
 
-  if file_mode_numeric.slice(2) == "1"
-    if file_permissions_symbolic[2].slice(2) == "x"
-      file_permissions_symbolic[2] = file_permissions_symbolic[2].gsub(/.$/, "t")
+  if file_mode_numeric.slice(2) == '1'
+    if file_permissions_symbolic[2].slice(2) == 'x'
+      file_permissions_symbolic[2] = file_permissions_symbolic[2].gsub(/.$/, 't')
     else
-      file_permissions_symbolic[2] = file_permissions_symbolic[2].gsub(/.$/, "T")
+      file_permissions_symbolic[2] = file_permissions_symbolic[2].gsub(/.$/, 'T')
     end
-  elsif file_mode_numeric.slice(2) == "2"
-    if file_permissions_symbolic[1].slice(2) == "x"
-      file_permissions_symbolic[1] = file_permissions_symbolic[1].gsub(/.$/, "s")
+  elsif file_mode_numeric.slice(2) == '2'
+    if file_permissions_symbolic[1].slice(2) == 'x'
+      file_permissions_symbolic[1] = file_permissions_symbolic[1].gsub(/.$/, 's')
     else
-      file_permissions_symbolic[1] = file_permissions_symbolic[1].gsub(/.$/, "S")
+      file_permissions_symbolic[1] = file_permissions_symbolic[1].gsub(/.$/, 'S')
     end
-  elsif file_mode_numeric.slice(2) == "4"
-    if file_permissions_symbolic[0].slice(2) == "x"
-      file_permissions_symbolic[0] = file_permissions_symbolic[0].gsub(/.$/, "s")
+  elsif file_mode_numeric.slice(2) == '4'
+    if file_permissions_symbolic[0].slice(2) == 'x'
+      file_permissions_symbolic[0] = file_permissions_symbolic[0].gsub(/.$/, 's')
     else
-      file_permissions_symbolic[0] = file_permissions_symbolic[0].gsub(/.$/, "S")
+      file_permissions_symbolic[0] = file_permissions_symbolic[0].gsub(/.$/, 'S')
     end
   end
 
@@ -132,9 +132,9 @@ end
 
 def get_last_modified_time(fs)
   if Time.now - fs.mtime >= (60 * 60 * 24 * (365 / 2.0)) || Time.now - fs.mtime < 0
-    fs.mtime.strftime("%_m %_d  %Y")
+    fs.mtime.strftime('%_m %_d  %Y')
   else
-    fs.mtime.strftime("%_m %_d %H:%M")
+    fs.mtime.strftime('%_m %_d %H:%M')
   end
 end
 
