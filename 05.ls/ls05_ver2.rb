3471cac4aabe3d2dfc2dc0f1d24fcde7ecb5702d
kazunoko1606 <djkazunoko@gmail.com>
2022-08-01 15:09:19 +0900

    変数名とメソッド名を修正

--- 05.ls/ls05_ver2.rb
+++ 05.ls/ls05_ver2.rb
@@ -20,68 +20,69 @@ MODE_MAP = {
 def exec
   params = ARGV.getopts('alr')
 
-  files = get_files(dotmatch: params['a'])
-  display_files(files, long_format: params['l'], reverse: params['r'])
+  paths = get_paths(dotmatch: params['a'])
+  list_paths(paths, long_format: params['l'], reverse: params['r'])
 end
 
-def display_files(files, long_format: false, reverse: false)
-  files = files.reverse if reverse
-  long_format ? display_long_format(files) : display_sort_by_column(files)
+def list_paths(paths, long_format: false, reverse: false)
+  paths = paths.reverse if reverse
+  long_format ? list_long(paths) : list_short(paths)
 end
 
-def get_files(dotmatch: false)
+def get_paths(dotmatch: false)
   dotmatch ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
 end
 
-def display_sort_by_column(files)
-  element_number = files.size.to_f
-  max_length = files.map(&:size).max
+def list_short(paths)
+  element_number = paths.size.to_f
+  max_length = paths.map(&:size).max
   row_number = (element_number / COLUMN_NUMBER).ceil
   lines = Array.new(row_number) { [] }
-  files.each_with_index do |file, index|
+  paths.each_with_index do |path, index|
     line_number = index % row_number
-    lines[line_number].push(file.ljust(max_length + 2))
+    lines[line_number].push(path.ljust(max_length + 2))
   end
   lines.each { |line| puts line.join }
 end
 
-def display_long_format(files)
-  long_formats = files.map { |file| get_long_format(file) }
+def list_long(paths)
+  long_formats = paths.map { |path| get_long_format(path) }
   max_length_map = get_max_length_map(long_formats)
-  number_of_blocks = long_formats.map { |long_format| long_format[:blocks] }.sum
-  puts "total #{number_of_blocks}"
+  block_total = long_formats.map { |long_format| long_format[:blocks] }.sum
+
+  puts "total #{block_total}"
   long_formats.each do |long_format|
     print "#{long_format[:type]}#{long_format[:mode]} "
-    print "#{long_format[:nlink].rjust(max_length_map[:link])} "
-    print "#{long_format[:username].ljust(max_length_map[:owner])}  "
-    print "#{long_format[:groupname].ljust(max_length_map[:group])}  "
-    print "#{long_format[:filesize].rjust(max_length_map[:filesize])} "
+    print "#{long_format[:nlink].rjust(max_length_map[:nlink])} "
+    print "#{long_format[:username].ljust(max_length_map[:username])}  "
+    print "#{long_format[:groupname].ljust(max_length_map[:groupname])}  "
+    print "#{long_format[:bitesize].rjust(max_length_map[:bitesize])} "
     print "#{long_format[:mtime]} "
     print "#{long_format[:pathname]}\n"
   end
 end
 
-def get_long_format(file)
-  file_stat = File.lstat(file)
+def get_long_format(path)
+  path_stat = File.lstat(path)
   {
-    type: format_type(file_stat.ftype),
-    mode: format_mode(file_stat.mode),
-    nlink: file_stat.nlink.to_s,
-    username: Etc.getpwuid(file_stat.uid).name,
-    groupname: Etc.getgrgid(file_stat.gid).name,
-    filesize: get_filesize(file_stat),
-    mtime: get_mtime(file_stat),
-    pathname: get_pathname(file),
-    blocks: file_stat.blocks
+    type: format_type(path_stat.ftype),
+    mode: format_mode(path_stat.mode),
+    nlink: path_stat.nlink.to_s,
+    username: Etc.getpwuid(path_stat.uid).name,
+    groupname: Etc.getgrgid(path_stat.gid).name,
+    bitesize: get_bitesize(path_stat),
+    mtime: get_mtime(path_stat),
+    pathname: get_pathname(path),
+    blocks: path_stat.blocks
   }
 end
 
 def get_max_length_map(long_formats)
   {
-    link: long_formats.map { |long_format| long_format[:nlink].size }.max,
-    owner: long_formats.map { |long_format| long_format[:username].size }.max,
-    group: long_formats.map { |long_format| long_format[:groupname].size }.max,
-    filesize: long_formats.map { |long_format| long_format[:filesize].size }.max
+    nlink: long_formats.map { |long_format| long_format[:nlink].size }.max,
+    username: long_formats.map { |long_format| long_format[:username].size }.max,
+    groupname: long_formats.map { |long_format| long_format[:groupname].size }.max,
+    bitesize: long_formats.map { |long_format| long_format[:bitesize].size }.max
   }
 end
 
@@ -128,27 +129,27 @@ def add_special_permissions(mode_octal, permissions_symbolic)
   permissions_symbolic
 end
 
-def get_filesize(file_stat)
-  if file_stat.rdev != 0
-    "#{file_stat.rdev_major}, #{file_stat.rdev_minor}"
+def get_bitesize(path_stat)
+  if path_stat.rdev != 0
+    "#{path_stat.rdev_major}, #{path_stat.rdev_minor}"
   else
-    file_stat.size.to_s
+    path_stat.size.to_s
   end
 end
 
-def get_mtime(file_stat)
-  if Time.now - file_stat.mtime >= (60 * 60 * 24 * (365 / 2.0)) || (Time.now - file_stat.mtime).negative?
-    file_stat.mtime.strftime('%_m %_d  %Y')
+def get_mtime(path_stat)
+  if Time.now - path_stat.mtime >= (60 * 60 * 24 * (365 / 2.0)) || (Time.now - path_stat.mtime).negative?
+    path_stat.mtime.strftime('%_m %_d  %Y')
   else
-    file_stat.mtime.strftime('%_m %_d %H:%M')
+    path_stat.mtime.strftime('%_m %_d %H:%M')
   end
 end
 
-def get_pathname(file)
-  if File.symlink?(file)
-    "#{file} -> #{File.readlink(file)}"
+def get_pathname(path)
+  if File.symlink?(path)
+    "#{path} -> #{File.readlink(path)}"
   else
-    file
+    path
   end
 end
 
