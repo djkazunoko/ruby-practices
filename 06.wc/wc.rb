dfa3df4c2085bdfad5a854b93675cb5ae2b4667e
kazunoko1606 <djkazunoko@gmail.com>
2022-08-05 14:52:18 +0900

    複数のファイルを表示

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -5,10 +5,7 @@ require 'optparse'
 
 def exec
   params = option_parse
-  row_count = get_row_count(ARGV[0]).to_s
-  word_count = get_word_count(ARGV[0]).to_s
-  filesize = File.size(ARGV[0]).to_s
-  print " #{row_count.rjust(7)} #{word_count.rjust(7)} #{filesize.rjust(7)} #{ARGV[0]}"
+  word_count(ARGV)
 end
 
 def option_parse
@@ -21,12 +18,39 @@ def option_parse
   params
 end
 
+def word_count(argv)
+  display_word_count(argv)
+  display_total_count(argv) if argv.size >= 2
+end
+
+def display_word_count(argv)
+  argv.each do |file|
+    row_count = get_row_count(file).to_s
+    word_count = get_word_count(file).to_s
+    filesize = File.size(file).to_s
+    puts " #{row_count.rjust(7)} #{word_count.rjust(7)} #{filesize.rjust(7)} #{file}"
+  end
+end
+
 def get_row_count(file)
   File.open(file) { |f| f.readlines.size }
 end
 
 def get_word_count(file)
   File.open(file) { |f| f.each.map { |line| line.split(/[ \t\n]+/).size}.sum }
+end
+
+def display_total_count(argv)
+  total_map = get_total_map(argv)
+  puts " #{total_map[:row_count].rjust(7)} #{total_map[:word_count].rjust(7)} #{total_map[:filesize].rjust(7)} total"
+end
+
+def get_total_map(argv)
+  {
+    row_count: argv.map { |file| get_row_count(file) }.sum.to_s,
+    word_count: argv.map { |file| get_word_count(file) }.sum.to_s,
+    filesize: argv.map { |file| File.size(file) }.sum.to_s
+  }
 end
 
 exec
