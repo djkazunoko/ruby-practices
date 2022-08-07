dadd811945d02e13eb17089d827337943667be2a
kazunoko1606 <djkazunoko@gmail.com>
2022-08-07 14:49:08 +0900

    メソッド名と変数名を修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -6,7 +6,7 @@ require 'optparse'
 def exec
   params = option_parse
   params = {c: true, l: true, w: true} if params.empty?
-  ARGV.empty? ? word_count_stdin(params) : word_count_files(params)
+  ARGV.empty? ? wc_stdin(params) : wc_files(params)
 end
 
 def option_parse
@@ -19,30 +19,30 @@ def option_parse
   params
 end
 
-def word_count_stdin(params)
-  array_of_line = $stdin.readlines
-  counts_map = build_counts_map(array_of_line)
+def wc_stdin(params)
+  array_of_lines = $stdin.readlines
+  counts_map = build_counts_map(array_of_lines)
   puts format_counts_map(counts_map, params).join
 end
 
-def build_counts_map(array_of_line)
+def build_counts_map(array_of_lines)
   {
-    line: count_line_number(array_of_line),
-    word: count_word_number(array_of_line),
-    byte: count_bytesize(array_of_line)
+    line: count_line_number(array_of_lines),
+    word: count_word_number(array_of_lines),
+    byte: count_bytesize(array_of_lines)
   }
 end
 
-def count_line_number(array_of_line)
-  array_of_line.size
+def count_line_number(array_of_lines)
+  array_of_lines.size
 end
 
-def count_word_number(array_of_line)
-  array_of_line.map { |line| line.split(/[ \t\n]+/).size }.sum
+def count_word_number(array_of_lines)
+  array_of_lines.map { |line| line.split(/[ \t\n]+/).size }.sum
 end
 
-def count_bytesize(array_of_line)
-  array_of_line.map { |line| line.bytesize }.sum
+def count_bytesize(array_of_lines)
+  array_of_lines.map { |line| line.bytesize }.sum
 end
 
 def format_counts_map(counts_map, params)
@@ -53,16 +53,16 @@ def format_counts_map(counts_map, params)
   formatted_data
 end
 
-def word_count_files(params)
-  arrays_of_line = files_to_arrays_of_line
-  counts_maps = arrays_of_line.map { |array_of_line| build_counts_map(array_of_line) }
+def wc_files(params)
+  arrays_of_lines = files_to_arrays_of_lines
+  counts_maps = arrays_of_lines.map { |array_of_lines| build_counts_map(array_of_lines) }
   counts_maps.each_with_index do |counts_map, idx|
     puts format_counts_map_with_path(counts_map, idx, params).join
   end
-  word_count_total(counts_maps, params) if ARGV.size >= 2
+  wc_total(counts_maps, params) if ARGV.size >= 2
 end
 
-def files_to_arrays_of_line
+def files_to_arrays_of_lines
   ARGV.map { |path| File.open(path) { |f| f.readlines } }
 end
 
@@ -71,7 +71,7 @@ def format_counts_map_with_path(counts_map, idx, params)
   format_counts_map(counts_map, params) << " #{path}"
 end
 
-def word_count_total(counts_maps, params)
+def wc_total(counts_maps, params)
   total_counts_map = build_total_counts_map(counts_maps)
   print_total_counts(total_counts_map, params)
 end
