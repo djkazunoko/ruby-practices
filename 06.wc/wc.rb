415ea2f17ce19b9faaa9379c25d5e7a8e32b2df5
kazunoko1606 <djkazunoko@gmail.com>
2022-08-07 13:41:17 +0900

    ファイル入力時にオプションを受け取る

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -6,7 +6,7 @@ require 'optparse'
 def exec
   params = option_parse
   params = {c: true, l: true, w: true} if params.empty?
-  ARGV.empty? ? word_count_stdin(params) : word_count_files
+  ARGV.empty? ? word_count_stdin(params) : word_count_files(params)
 end
 
 def option_parse
@@ -53,27 +53,27 @@ def format_counts_map(counts_map, params)
   formatted_data
 end
 
-def word_count_files
+def word_count_files(params)
   arrays_of_line = files_to_arrays_of_line
   counts_maps = arrays_of_line.map { |array_of_line| build_counts_map(array_of_line) }
   counts_maps.each_with_index do |counts_map, idx|
-    puts format_counts_map_with_path(counts_map, idx).join
+    puts format_counts_map_with_path(counts_map, idx, params).join
   end
-  word_count_total(counts_maps) if ARGV.size >= 2
+  word_count_total(counts_maps, params) if ARGV.size >= 2
 end
 
 def files_to_arrays_of_line
   ARGV.map { |path| File.open(path) { |f| f.readlines } }
 end
 
-def format_counts_map_with_path(counts_map, idx)
+def format_counts_map_with_path(counts_map, idx, params)
   path = ARGV[idx]
-  format_counts_map(counts_map).push " #{path}"
+  format_counts_map(counts_map, params) << " #{path}"
 end
 
-def word_count_total(counts_maps)
+def word_count_total(counts_maps, params)
   total_counts_map = build_total_counts_map(counts_maps)
-  print_total_counts(total_counts_map)
+  print_total_counts(total_counts_map, params)
 end
 
 def build_total_counts_map(counts_maps)
@@ -84,13 +84,13 @@ def build_total_counts_map(counts_maps)
   }
 end
 
-def print_total_counts(total_counts_map)
-  print [
-    " #{total_counts_map[:line].to_s.rjust(7)}",
-    " #{total_counts_map[:word].to_s.rjust(7)}",
-    " #{total_counts_map[:byte].to_s.rjust(7)}",
-    " total\n"
-  ].join
+def print_total_counts(total_counts_map, params)
+  formatted_data = []
+  formatted_data << " #{total_counts_map[:line].to_s.rjust(7)}" if params[:l]
+  formatted_data << " #{total_counts_map[:word].to_s.rjust(7)}" if params[:w]
+  formatted_data << " #{total_counts_map[:byte].to_s.rjust(7)}" if params[:c]
+  formatted_data << " total\n"
+  print formatted_data.join
 end
 
 exec
