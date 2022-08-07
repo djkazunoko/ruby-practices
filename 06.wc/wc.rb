c3730f2ac4a4bbd3757aab901062066b7d354917
kazunoko1606 <djkazunoko@gmail.com>
2022-08-07 12:02:46 +0900

    合計出力処理を実装

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -11,15 +11,15 @@ end
 
 def word_count_stdin
   array_of_line = $stdin.readlines
-  counts_map = build_counts(array_of_line)
-  puts format_counts(counts_map).join
+  counts_map = build_counts_map(array_of_line)
+  puts format_counts_map(counts_map).join
 end
 
-def build_counts(array_of_line)
+def build_counts_map(array_of_line)
   {
-    line: count_line_number(array_of_line).to_s,
-    word: count_word_number(array_of_line).to_s,
-    byte: count_bytesize(array_of_line).to_s
+    line: count_line_number(array_of_line),
+    word: count_word_number(array_of_line),
+    byte: count_bytesize(array_of_line)
   }
 end
 
@@ -35,29 +35,52 @@ def count_bytesize(array_of_line)
   array_of_line.map { |line| line.bytesize }.sum
 end
 
-def format_counts(counts_map)
+def format_counts_map(counts_map)
   [
-    " #{counts_map[:line].rjust(7)}",
-    " #{counts_map[:word].rjust(7)}",
-    " #{counts_map[:byte].rjust(7)}"
+    " #{counts_map[:line].to_s.rjust(7)}",
+    " #{counts_map[:word].to_s.rjust(7)}",
+    " #{counts_map[:byte].to_s.rjust(7)}"
   ]
 end
 
 def word_count_files
   arrays_of_line = files_to_arrays_of_line
-  counts_maps = arrays_of_line.map { |array_of_line| build_counts(array_of_line) }
+  counts_maps = arrays_of_line.map { |array_of_line| build_counts_map(array_of_line) }
   counts_maps.each_with_index do |counts_map, idx|
-    puts format_counts_with_path(counts_map, idx).join
+    puts format_counts_map_with_path(counts_map, idx).join
   end
+  word_count_total(counts_maps) if ARGV.size >= 2
 end
 
 def files_to_arrays_of_line
   ARGV.map { |path| File.open(path) { |f| f.readlines } }
 end
 
-def format_counts_with_path(counts_map, idx)
+def format_counts_map_with_path(counts_map, idx)
   path = ARGV[idx]
-  format_counts(counts_map).push " #{path}"
+  format_counts_map(counts_map).push " #{path}"
+end
+
+def word_count_total(counts_maps)
+  total_counts_map = build_total_counts_map(counts_maps)
+  print_total_counts(total_counts_map)
+end
+
+def build_total_counts_map(counts_maps)
+  {
+    line: counts_maps.map { |counts_map| counts_map[:line] }.sum,
+    word: counts_maps.map { |counts_map| counts_map[:word] }.sum,
+    byte: counts_maps.map { |counts_map| counts_map[:byte] }.sum
+  }
+end
+
+def print_total_counts(total_counts_map)
+  print [
+    " #{total_counts_map[:line].to_s.rjust(7)}",
+    " #{total_counts_map[:word].to_s.rjust(7)}",
+    " #{total_counts_map[:byte].to_s.rjust(7)}",
+    " total\n"
+  ].join
 end
 
 exec
