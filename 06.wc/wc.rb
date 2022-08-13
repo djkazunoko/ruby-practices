06bf8c3196e909b969a3d80f6f166062732aec5b
kazunoko1606 <djkazunoko@gmail.com>
2022-08-14 06:59:13 +0900

    paramsをoptionsに変更

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,25 +4,25 @@
 require 'optparse'
 
 def exec
-  params = option_parse
-  params = { c: true, l: true, w: true } if params.empty?
-  ARGV.empty? ? wc_stdin(params) : wc_files(params)
+  options = parse_options
+  options = { c: true, l: true, w: true } if options.empty?
+  ARGV.empty? ? wc_stdin(options) : wc_files(options)
 end
 
-def option_parse
+def parse_options
   opt = OptionParser.new
-  params = {}
+  options = {}
   opt.on('-c')
   opt.on('-l')
   opt.on('-w')
-  opt.parse!(ARGV, into: params)
-  params
+  opt.parse!(ARGV, into: options)
+  options
 end
 
-def wc_stdin(params)
+def wc_stdin(options)
   array_of_lines = $stdin.readlines
   counts_map = build_counts_map(array_of_lines)
-  puts format_counts_map(counts_map, params).join
+  puts format_counts_map(counts_map, options).join
 end
 
 def build_counts_map(array_of_lines)
@@ -45,21 +45,21 @@ def count_bytesize(array_of_lines)
   array_of_lines.map(&:bytesize).sum
 end
 
-def format_counts_map(counts_map, params)
+def format_counts_map(counts_map, options)
   formatted_data = []
-  formatted_data << " #{counts_map[:line].to_s.rjust(7)}" if params[:l]
-  formatted_data << " #{counts_map[:word].to_s.rjust(7)}" if params[:w]
-  formatted_data << " #{counts_map[:byte].to_s.rjust(7)}" if params[:c]
+  formatted_data << " #{counts_map[:line].to_s.rjust(7)}" if options[:l]
+  formatted_data << " #{counts_map[:word].to_s.rjust(7)}" if options[:w]
+  formatted_data << " #{counts_map[:byte].to_s.rjust(7)}" if options[:c]
   formatted_data
 end
 
-def wc_files(params)
+def wc_files(options)
   arrays_of_lines = files_to_arrays_of_lines
   counts_maps = arrays_of_lines.map { |array_of_lines| build_counts_map(array_of_lines) }
   counts_maps.each_with_index do |counts_map, idx|
-    puts format_counts_map_with_path(counts_map, idx, params).join
+    puts format_counts_map_with_path(counts_map, idx, options).join
   end
-  wc_total(counts_maps, params) if ARGV.size >= 2
+  wc_total(counts_maps, options) if ARGV.size >= 2
 end
 
 def files_to_arrays_of_lines
@@ -67,14 +67,14 @@ def files_to_arrays_of_lines
   ARGV.map { |path| File.open(path).call(&:readlines) }
 end
 
-def format_counts_map_with_path(counts_map, idx, params)
+def format_counts_map_with_path(counts_map, idx, options)
   path = ARGV[idx]
-  format_counts_map(counts_map, params) << " #{path}"
+  format_counts_map(counts_map, options) << " #{path}"
 end
 
-def wc_total(counts_maps, params)
+def wc_total(counts_maps, options)
   total_counts_map = build_total_counts_map(counts_maps)
-  print_total_counts(total_counts_map, params)
+  print_total_counts(total_counts_map, options)
 end
 
 def build_total_counts_map(counts_maps)
@@ -85,11 +85,11 @@ def build_total_counts_map(counts_maps)
   }
 end
 
-def print_total_counts(total_counts_map, params)
+def print_total_counts(total_counts_map, options)
   formatted_data = []
-  formatted_data << " #{total_counts_map[:line].to_s.rjust(7)}" if params[:l]
-  formatted_data << " #{total_counts_map[:word].to_s.rjust(7)}" if params[:w]
-  formatted_data << " #{total_counts_map[:byte].to_s.rjust(7)}" if params[:c]
+  formatted_data << " #{total_counts_map[:line].to_s.rjust(7)}" if options[:l]
+  formatted_data << " #{total_counts_map[:word].to_s.rjust(7)}" if options[:w]
+  formatted_data << " #{total_counts_map[:byte].to_s.rjust(7)}" if options[:c]
   formatted_data << " total\n"
   print formatted_data.join
 end
