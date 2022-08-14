2a5806facb6067b43be77d29730485bdfa48af12
kazunoko1606 <djkazunoko@gmail.com>
2022-08-14 14:58:54 +0900

    変数名とメソッド名を修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -7,7 +7,7 @@ def exec
   options = parse_options
   options = { c: true, l: true, w: true } if options.empty?
   paths = ARGV
-  paths.empty? ? wc_stdin(options) : wc_files(options, paths)
+  paths.empty? ? count_stdin(options) : count_files(options, paths)
 end
 
 def parse_options
@@ -20,7 +20,7 @@ def parse_options
   options
 end
 
-def wc_stdin(options)
+def count_stdin(options)
   lines = $stdin.readlines
   file_data = build_counts_map(lines)
   puts format_file_data(file_data, options).join
@@ -28,17 +28,17 @@ end
 
 def build_counts_map(lines)
   {
-    line_number: count_line_number(lines),
-    word_number: count_word_number(lines),
+    number_of_lines: count_line(lines),
+    number_of_words: count_word(lines),
     bytesize: count_bytesize(lines)
   }
 end
 
-def count_line_number(lines)
+def count_line(lines)
   lines.size
 end
 
-def count_word_number(lines)
+def count_word(lines)
   lines.sum { |line| line.split(/[ \t\n]+/).size }
 end
 
@@ -48,21 +48,21 @@ end
 
 def format_file_data(file_data, options)
   formatted_file_data = []
-  formatted_file_data << " #{file_data[:line_number].to_s.rjust(7)}" if options[:l]
-  formatted_file_data << " #{file_data[:word_number].to_s.rjust(7)}" if options[:w]
+  formatted_file_data << " #{file_data[:number_of_lines].to_s.rjust(7)}" if options[:l]
+  formatted_file_data << " #{file_data[:number_of_words].to_s.rjust(7)}" if options[:w]
   formatted_file_data << " #{file_data[:bytesize].to_s.rjust(7)}" if options[:c]
   formatted_file_data << " #{file_data[:path]}" if file_data.has_key?(:path)
   formatted_file_data
 end
 
-def wc_files(options, paths)
-  files = get_files(paths)
-  files_data = get_files_data(files)
+def count_files(options, paths)
+  files = build_files(paths)
+  files_data = build_files_data(files)
   files_data.map { |file_data| puts format_file_data(file_data, options).join}
-  wc_total(files_data, options) if paths.size >= 2
+  count_total(files_data, options) if paths.size >= 2
 end
 
-def get_files(paths)
+def build_files(paths)
   paths.map do |path|
     {
       lines: File.readlines(path),
@@ -71,7 +71,7 @@ def get_files(paths)
   end
 end
 
-def get_files_data(files)
+def build_files_data(files)
   files.map do |file|
     file_data = build_counts_map(file[:lines])
     file_data[:path] = file[:path]
@@ -79,15 +79,15 @@ def get_files_data(files)
   end
 end
 
-def wc_total(files_data, options)
+def count_total(files_data, options)
   total_file_data = build_total_counts_map(files_data)
   puts format_file_data(total_file_data, options).push(' total').join
 end
 
 def build_total_counts_map(files_data)
   {
-    line_number: files_data.sum { |file_data| file_data[:line_number] },
-    word_number: files_data.sum { |file_data| file_data[:word_number] },
+    number_of_lines: files_data.sum { |file_data| file_data[:number_of_lines] },
+    number_of_words: files_data.sum { |file_data| file_data[:number_of_words] },
     bytesize: files_data.sum { |file_data| file_data[:bytesize] }
   }
 end
