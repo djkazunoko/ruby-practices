b49e095a9e1df7b65a69d83fa462c44657e477ce
djkazunoko <djkazunoko@gmail.com>
2022-10-20 11:20:31 +0900

    rubocop修正

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'frame'
 
 class Game
@@ -7,23 +9,23 @@ class Game
 
   def score
     @game_score = 0
-    (0..9).each do |n|
-      @frame = Frame.new(@frames[n])
+    (0..9).each do |idx|
+      @frame = Frame.new(@frames[idx])
       @game_score += @frame.score
-      @frames[n + 1] ||= []
-      @frames[n + 2] ||= []
-      add_bonus_point(n)
+      @frames[idx + 1] ||= []
+      @frames[idx + 2] ||= []
+      add_bonus_point(idx)
     end
     @game_score
   end
 
-  def add_bonus_point(n)
+  def add_bonus_point(idx)
     if @frame.strike?
-      next_two_shots = (@frames[n + 1] + @frames[n + 2]).slice(0, 2)
+      next_two_shots = (@frames[idx + 1] + @frames[idx + 2]).slice(0, 2)
       bonus_point = next_two_shots.sum { |s| Shot.new(s).score }
       @game_score += bonus_point
     elsif @frame.spare?
-      next_shot = @frames[n + 1][0]
+      next_shot = @frames[idx + 1][0]
       bonus_point = Shot.new(next_shot).score
       @game_score += bonus_point
     end
