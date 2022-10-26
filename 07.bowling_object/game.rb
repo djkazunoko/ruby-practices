cbaf87093a6b56ea8cf26f7a85dc6225944922da
djkazunoko <djkazunoko@gmail.com>
2022-10-26 15:02:18 +0900

    スペアとストライクの加点処理を修正

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -14,20 +14,20 @@ class Game
       @game_score += @frame.score
       @frames[idx + 1] ||= []
       @frames[idx + 2] ||= []
-      add_bonus_point(idx)
+      @game_score += calc_bonus_point(idx)
     end
     @game_score
   end
 
-  def add_bonus_point(idx)
+  def calc_bonus_point(idx)
     if @frame.strike?
       next_two_shots = (@frames[idx + 1] + @frames[idx + 2]).slice(0, 2)
       bonus_point = next_two_shots.sum { |s| Shot.new(s).score }
-      @game_score += bonus_point
     elsif @frame.spare?
       next_shot = @frames[idx + 1][0]
       bonus_point = Shot.new(next_shot).score
-      @game_score += bonus_point
+    else
+      bonus_point = 0
     end
   end
 end
