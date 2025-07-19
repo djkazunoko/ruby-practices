1be8ae931e31dcd45ee326b4c58163f5284d3344
djkazunoko <djkazunoko@gmail.com>
2025-07-19 11:24:27 +0900

    月の全ての日付を表示

--- 02.calendar/cal-review.rb
+++ 02.calendar/cal-review.rb
@@ -6,7 +6,14 @@ today = Date.today
 params["m"] ||= today.month
 params["y"] ||= today.year
 
-month = params["m"]
-year = params["y"]
+month = params["m"].to_i
+year = params["y"].to_i
 
 puts "#{month}月 #{year}"
+puts '日 月 火 水 木 金 土'
+
+last_day = Date.new(year, month, -1).day
+
+(1..last_day).each do |n|
+  print n.to_s.rjust(2)
+end
