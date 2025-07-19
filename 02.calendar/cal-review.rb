60e6d2bcbb084cc34a59dcd58f0d81ef0d52bd5a
djkazunoko <djkazunoko@gmail.com>
2025-07-19 10:57:23 +0900

    引数が指定されない場合は今月・今年を表示

--- 02.calendar/cal-review.rb
+++ 02.calendar/cal-review.rb
@@ -1,6 +1,10 @@
 require 'optparse'
+require 'date'
 
 params = ARGV.getopts("m:y:")
+today = Date.today
+params["m"] ||= today.month
+params["y"] ||= today.year
 
 month = params["m"]
 year = params["y"]
