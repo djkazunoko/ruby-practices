71667a4339ede4df357470a9c8ae7e0db6f8d372
djkazunoko <djkazunoko@gmail.com>
2025-07-31 13:28:48 +0900

    参考実装を反映してリファクタリング

--- 04.bowling/bowling-review.rb
+++ 04.bowling/bowling-review.rb
@@ -1,39 +1,44 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.split(',')
-shots = []
-scores.each do |s|
-  if s == 'X'
-    shots << 10
-    shots << 0
-  else
-    shots << s.to_i
-  end
-end
+def parse_marks(marks)
+  pins = marks.split(',').map { |m| m == 'X' ? 10 : m.to_i }
 
-frames = shots.each_slice(2).to_a
+  frames = []
+  frame = []
+  pins.each do |p|
+    frame << p
 
-def strike?(frame)
-  frame[0] == 10
-end
-
-def spare?(frame)
-  frame.sum == 10
-end
-
-frames.each_with_index do |frame, idx|
-  next_shot = frames[idx + 1][0]
-  after_next_shot = next_shot == 10 ? frames[idx + 2][0] : frames[idx + 1][1]
-
-  if strike?(frame)
-    frames[idx][0] = frame[0] + next_shot + after_next_shot
-  elsif spare?(frame)
-    frames[idx][0] = frame[0] + next_shot
+    if frames.length < 10
+      if frame.length >= 2 || p == 10
+        frames << frame.dup
+        frame.clear
+      end
+    else
+      frames.last << p
+    end
   end
 
-  break if idx == 8
+  frames
 end
 
-puts frames.flatten.sum
+def score_for(frames)
+  score = 0
+  (0..9).each do |n|
+    frame, next_frame, after_next_frame = frames.slice(n, 3)
+    next_frame ||= []
+    after_next_frame ||= []
+
+    score += if frame[0] == 10 # strike
+               frame.sum + (next_frame + after_next_frame).slice(0, 2).sum
+             elsif frame.sum == 10 # spare
+               frame.sum + next_frame[0]
+             else
+               frame.sum
+             end
+  end
+  score
+end
+
+frames = parse_marks(ARGV[0])
+puts score_for(frames)
